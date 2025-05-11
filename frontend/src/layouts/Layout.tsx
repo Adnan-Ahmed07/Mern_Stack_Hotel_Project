0aae1f7887e4755a640a8a2c836c00c660bd98d1
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-05-11 09:52:10 -0600

    test layout created

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/Header";
+import { Outlet } from "react-router-dom";
+
+
+
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+    <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
